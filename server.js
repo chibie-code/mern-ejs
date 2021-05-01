6da01c0c129e1726f85405e764bb1adf1cf576e1
chibie-code <76469653+chibie-code@users.noreply.github.com>
2021-05-01 08:30:26 -0700

    added the socket logic

--- server.js
+++ server.js
@@ -1,48 +1,4 @@
 /*
-const nouns = ["appearance", "appointment", "argument", "arrival", "awareness", "cabinet", "candidate", "charity", "cigarette", "committee", "conclusion", "confusion", "customer", "direction", "director", "discussion", "editor", "emotion", "employee", "employment", "energy", "family", "government", "importance", "industry", "inflation", "management", "manager", "measurement", "media", "medicine", "membership", "memory", "passenger", "percentage", "perception", "poetry", "possession", "preference", "president", "procedure", "professor", "quality", "quantity", "ratio", "relation", "requirement", "revenue", "solution", "sympathy", "actor", "area", "aspect", "audience", "birthday", "category", "chemistry", "city", "climate", "complaint", "context", "contribution", "courage", "currency", "dealer", "departure", "depression", "description", "device", "enthusiasm", "equipment", "establishment", "family", "feedback", "fishing", "history", "improvement", "inflation", "injury", "maintenance", "marriage", "office", "penalty", "percentage", "permission", "possession", "reading", "reality", "recommendation", "relation", "republic", "salad", "secretary", "selection", "software", "system", "television", "theory", "transportation", "writing"];
-const adj = ["abrupt", "accidental", "agreeable", "animated", "bashful", "calm", "careless", "chunky", "coordinated", "difficult", "disturbed", "drab", "drunk", "efficient", "equable", "far", "first", "grouchy", "incandescent", "joyous", "kindly", "lethal", "long", "loud", "loving", "married", "materialistic", "obeisant", "optimal", "perpetual", "premium", "public", "regular", "righteous", "sexy", "shy", "skillful", "slippery", "spiky", "subdued", "successfully", "tender", "unable", "unsightly", "various", "victorious", "wide-eyed", "witty", "wooden", "yielding", "yummy", "abandoned", "abject", "absent", "attractive", "auspicious", "bewildered", "blushing", "boorish", "bored", "boundless", "caring", "chief", "cooing", "dull", "earsplitting", "efficacious", "eight", "embarrassed", "entertaining", "faded", "few", "filthy", "five", "giant", "graceful", "guttural", "highfalutin", "knowledgeable", "light", "limping", "loutish", "magnificent", "next", "obvious", "ordinary", "overt", "parallel", "pastoral", "scrawny", "screeching", "silky", "slippery", "slow", "smoggy", "strict", "swift", "unaccountable", "vague", "wet", "wooden"];
-const express = require('express');
-
-const ejs = require('ejs');
-
-var rug = require('random-username-generator');
-
-rug.setSeperator('.');
-
-rug.setNames([...nouns]);
-rug.setAdjectives([...adj]);
-
-// const cors = require('cors');
-
-const app = express();
-
-var messages_array = [];
-
-const server = app.listen(8080, () => { // the http server
-    console.log('Server listening at port 8080');
-});
-
-const io = require("socket.io")(server);
-
-function dateTime() { // date-time timestamp function. returns final date-time object
-
-    const timestampNow = Date.now() // get current timestamp in milliseconds
-    const date = new Date(timestampNow); // convert timestamp to date object
-
-    const humanDateFormat = date.toLocaleString(); // then convert fate object to human readable format
-
-    const splitDateTime = humanDateFormat.split(', '); // split date and time. date at idx 0 and time at idx 1
-
-    const finalDateTime = {
-        date: splitDateTime[0],
-        time: splitDateTime[1] + ' UTC'
-    };
-    // console.log('finalDateTime:',finalDateTime);
-    return finalDateTime; // obj => { date: '', time: '' }
-}
-
-// set the view engine to ejs
-app.set('view engine', 'ejs');
 
 // allow the public folder to be accessible to the ejs files as well as the public via their uris with the '/' being the '/public' folder directory
 app.use(express.static(__dirname + '/public'));
@@ -70,6 +26,45 @@ app.get('/socket_demo', function(req, res) {
 app.use(function(req, res) {
     res.status(404).render('404');
 });
+
+
+*/
+
+const nouns = ["appearance", "appointment", "argument", "arrival", "awareness", "cabinet", "candidate", "charity", "cigarette", "committee", "conclusion", "confusion", "customer", "direction", "director", "discussion", "editor", "emotion", "employee", "employment", "energy", "family", "government", "importance", "industry", "inflation", "management", "manager", "measurement", "media", "medicine", "membership", "memory", "passenger", "percentage", "perception", "poetry", "possession", "preference", "president", "procedure", "professor", "quality", "quantity", "ratio", "relation", "requirement", "revenue", "solution", "sympathy", "actor", "area", "aspect", "audience", "birthday", "category", "chemistry", "city", "climate", "complaint", "context", "contribution", "courage", "currency", "dealer", "departure", "depression", "description", "device", "enthusiasm", "equipment", "establishment", "family", "feedback", "fishing", "history", "improvement", "inflation", "injury", "maintenance", "marriage", "office", "penalty", "percentage", "permission", "possession", "reading", "reality", "recommendation", "relation", "republic", "salad", "secretary", "selection", "software", "system", "television", "theory", "transportation", "writing"];
+const adj = ["abrupt", "accidental", "agreeable", "animated", "bashful", "calm", "careless", "chunky", "coordinated", "difficult", "disturbed", "drab", "drunk", "efficient", "equable", "far", "first", "grouchy", "incandescent", "joyous", "kindly", "lethal", "long", "loud", "loving", "married", "materialistic", "obeisant", "optimal", "perpetual", "premium", "public", "regular", "righteous", "sexy", "shy", "skillful", "slippery", "spiky", "subdued", "successfully", "tender", "unable", "unsightly", "various", "victorious", "wide-eyed", "witty", "wooden", "yielding", "yummy", "abandoned", "abject", "absent", "attractive", "auspicious", "bewildered", "blushing", "boorish", "bored", "boundless", "caring", "chief", "cooing", "dull", "earsplitting", "efficacious", "eight", "embarrassed", "entertaining", "faded", "few", "filthy", "five", "giant", "graceful", "guttural", "highfalutin", "knowledgeable", "light", "limping", "loutish", "magnificent", "next", "obvious", "ordinary", "overt", "parallel", "pastoral", "scrawny", "screeching", "silky", "slippery", "slow", "smoggy", "strict", "swift", "unaccountable", "vague", "wet", "wooden"];
+
+var express = require('express');
+var app = express();
+
+var rug = require('random-username-generator');
+
+const io = require("socket.io")(server);
+
+rug.setSeperator('.');
+
+rug.setNames([...nouns]);
+rug.setAdjectives([...adj]);
+
+const app = express();
+
+var messages_array = [];
+
+function dateTime() { // date-time timestamp function. returns final date-time object
+
+    const timestampNow = Date.now() // get current timestamp in milliseconds
+    const date = new Date(timestampNow); // convert timestamp to date object
+
+    const humanDateFormat = date.toLocaleString(); // then convert fate object to human readable format
+
+    const splitDateTime = humanDateFormat.split(', '); // split date and time. date at idx 0 and time at idx 1
+
+    const finalDateTime = {
+        date: splitDateTime[0],
+        time: splitDateTime[1] + ' UTC'
+    };
+    // console.log('finalDateTime:',finalDateTime);
+    return finalDateTime; // obj => { date: '', time: '' }
+}
 
 io.on('connection', (socket) => {
     let userDATA = {
@@ -120,11 +115,6 @@ io.on('connection', (socket) => {
     });
 
 });
-*/
-
-
-var express = require('express');
-var app = express();
 
 // set the port of our application
 // process.env.PORT lets the port be set by Heroku
