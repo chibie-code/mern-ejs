191c619403f43529bd970234ca65119d2880d3a9
chibie-code <76469653+chibie-code@users.noreply.github.com>
2021-05-01 08:18:49 -0700

    added about page

--- server.js
+++ server.js
@@ -143,6 +143,13 @@ app.get('/', function(req, res) {
     res.render('index');
 });
 
+// set the about page route
+app.get('/about', function(req, res) {
+
+    // ejs render automatically looks in the views folder
+    res.render('about');
+});
+
 app.listen(port, function() {
     console.log('Our app is running on http://localhost:' + port);
 });
